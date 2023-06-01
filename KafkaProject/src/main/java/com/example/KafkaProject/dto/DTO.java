3f181ed65081dbe2728a544e63f204b2b11b53ae
Kat-G <katylaky@outlook.com>
2023-06-01 10:48:06 +0300

    Add miss annotation

--- KafkaProject/src/main/java/com/example/KafkaProject/dto/DTO.java
+++ KafkaProject/src/main/java/com/example/KafkaProject/dto/DTO.java
@@ -2,8 +2,10 @@ package com.example.KafkaProject.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class DTO {
